90d67b4f5b68695a75f50fce54d00b43b70d070a
Christian Grothoff <christian@grothoff.org>
2005-04-20 01:31:49 +0000

    gpl

--- src/plugins/printable/dictionary-builder.c
+++ src/plugins/printable/dictionary-builder.c
@@ -1,3 +1,27 @@
+/*
+     This file is part of libextractor.
+     (C) 2002, 2003, 2004 Vidyut Samanta and Christian Grothoff
+
+     libextractor is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 2, or (at your
+     option) any later version.
+
+     libextractor is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with libextractor; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+
+     Portions of this code were adapted from libhtmlparse by
+     Mooneer Salem (mooneer@translator.cs).  The main changes
+     to libhtmlparse were the removal of globals to make the
+     code reentrant.
+ */
 /**
  * Tool to build a bloomfilter from a dictionary.
  */
