b7c431ecb2a46da536e60239ad567facbf069f18
Christian Grothoff <christian@grothoff.org>
2005-05-08 08:36:39 +0000

    update

--- src/main/libextractor_python_setup.py
+++ src/main/libextractor_python_setup.py
@@ -1,9 +1,13 @@
 from distutils.core import Extension, setup
+import sys
+
+path=sys.argv[0]
+sys.argv = sys.argv[1:]
 
 cmod = Extension("extractor",["libextractor_python3.c"],
                  libraries=["extractor"],
                  include_dirs=["../include"],
-                 library_dirs=["/home/heiko/usr/lib"])
+                 library_dirs=[path])
 
 setup(name="Extractor",
       version="0.5.0",
