07655896cad10d25f407714341fb07e2a7c5bba0
Christian Grothoff <christian@grothoff.org>
2005-07-03 19:52:16 +0000

    fixes

---
+++ src/plugins/exiv2/exv_msvc.h
@@ -0,0 +1,41 @@
+/* ***************************************************************** -*- C -*- */
+/*!
+  @file    exv_msvc.h
+  @brief   Configuration settings for MSVC
+  @version $Rev$
+  @author  Andreas Huggel (ahu)
+           <a href="mailto:ahuggel@gmx.net">ahuggel@gmx.net</a>
+  @date    07-Feb-04, ahu: created
+           26-Feb-05, ahu: renamed and moved to src directory
+ */
+
+/* Todo: The PACKAGE_* defines should be generated */
+
+#ifdef _MSC_VER
+
+/* Define to 1 if you have the <process.h> header file. */
+#define EXV_HAVE_PROCESS_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define EXV_PACKAGE_BUGREPORT "ahuggel@gmx.net"
+
+/* Define to the full name of this package. */
+#define EXV_PACKAGE_NAME "Exiv2"
+
+/* Define to the full name and version of this package. */
+#define EXV_PACKAGE_STRING "Exiv2 0.7"
+
+/* Define to the one symbol short name of this package. */
+#define EXV_PACKAGE_TARNAME "exiv2"
+
+/* Define to the version of this package. */
+#define EXV_PACKAGE_VERSION "0.7"
+
+/* Define to `int' if <sys/types.h> does not define pid_t. */
+#define pid_t int
+
+/* File path seperator */
+#define EXV_SEPERATOR_STR "\\"
+#define EXV_SEPERATOR_CHR '\\'
+
+#endif /* _MSC_VER */
