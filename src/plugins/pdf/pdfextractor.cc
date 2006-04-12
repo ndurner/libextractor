6f322d688b606d51d713ac1c0b6265a617152312
Christian Grothoff <christian@grothoff.org>
2006-04-12 01:01:19 +0000

    hack

--- src/plugins/pdf/pdfextractor.cc
+++ src/plugins/pdf/pdfextractor.cc
@@ -84,7 +84,16 @@ extern "C" {
 	for (pos=0;pos<len;pos+=2) {
 	  u[0] = s1->getChar(pos+1);
 	  u[1] = s1->getChar(pos);
+	  /* Q: is there a difference between UTF-16 and UNICODE?
+	     Which one is needed here?  And how to do it on solaris
+	     where UNICODE is not known!?
+	     See http://lists.gnu.org/archive/html/libextractor/2006-04/msg00006.html
+	  */
+#ifdef SOLARIS
+	  con = (char*) convertToUtf8((const char*) u, 2, "UTF-16");
+#else
 	  con = (char*) convertToUtf8((const char*) u, 2, "UNICODE");
+#endif
 	  strcat(result, con);
 	  free(con);
 	}
