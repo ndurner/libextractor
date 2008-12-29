f867ff64bf60e94437046dece8b2be033c24729a
Heikki Lindholm <holin@iki.fi>
2008-12-29 11:01:30 +0000

    copyright headers

--- src/intlemu/intlemu.c
+++ src/intlemu/intlemu.c
@@ -1,3 +1,13 @@
+/*
+ *  libintlemu - A Core Foundation libintl emulator
+ *  Copyright (C) 2008  Heikki Lindholm <holin@iki.fi>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ */
+
 #include <CoreFoundation/CoreFoundation.h>
 #include <stdlib.h>
 #include <pthread.h>
--- src/intlemu/libintlemu.h
+++ src/intlemu/libintlemu.h
@@ -1,4 +1,12 @@
-/* Mac OS X CoreFoundation libintl emulator */
+/*
+ *  libintlemu - A Core Foundation libintl emulator
+ *  Copyright (C) 2008  Heikki Lindholm <holin@iki.fi>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ */
 
 #ifndef LIBINTLEMU_H
 #define LIBINTLEMU_H
