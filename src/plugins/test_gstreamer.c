eabea63a7943baa3694bb30b42dea57c3b9165b9
LRN <lrn1986@gmail.com>
2012-08-16 03:40:32 +0000

    Stub test for GStreamer

---
+++ src/plugins/test_gstreamer.c
@@ -0,0 +1,43 @@
+/*
+     This file is part of libextractor.
+     (C) 2012 Vidyut Samanta and Christian Grothoff
+
+     libextractor is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 3, or (at your
+     option) any later version.
+
+     libextractor is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with libextractor; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+/**
+ * @file plugins/test_gstreamer.c
+ * @brief testcase for gstreamer plugin
+ * @author LRN
+ */
+#include "platform.h"
+#include "test_lib.h"
+
+
+/**
+ * Main function for the GStreamer testcase.
+ *
+ * @param argc number of arguments (ignored)
+ * @param argv arguments (ignored)
+ * @return 0 on success
+ */
+int
+main (int argc, char *argv[])
+{
+  /* FIXME: implement a test */
+  return 0;
+}
+
+/* end of test_gstreamer.c */
