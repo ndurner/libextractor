bfe7656c6e8c9cf666270d77dc91bad5135292a0
Christian Grothoff <christian@grothoff.org>
2012-09-22 17:02:06 +0000

    -fixing OSX build issues

--- src/main/extractor_plugpath.c
+++ src/main/extractor_plugpath.c
@@ -257,13 +257,14 @@ get_path_from_dyld_image ()
 {
   const char *path;
   char *s;
+  char *p;
   unsigned int i;
   int c;
 
   c = _dyld_image_count ();
   for (i = 0; i < c; i++) 
     {
-      if (_dyld_get_image_header (i) != &_mh_dylib_header)
+      if (((void *) _dyld_get_image_header (i)) != (void *) &_mh_dylib_header)
 	continue;
       path = _dyld_get_image_name (i);
       if ( (NULL == path) || (0 == strlen (path)) )
